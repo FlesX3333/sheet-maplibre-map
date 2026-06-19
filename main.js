bd660ec96d6769918bda7cb3692855fc781b7c52
FlesX3333 <109177508+FlesX3333@users.noreply.github.com>
2026-06-19 04:51:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,122 @@
+const map = new maplibregl.Map({
+  container: 'map',
+  style: "https://raw.githubusercontent.com/gtitov/basemaps/refs/heads/master/positron-nolabels.json",
+  center: [100, 50],
+  zoom: 2,
+  hash: true
+});
+
+map.on("load", () => {
+  fetch("https://docs.google.com/spreadsheets/d/1f0waZduz5CXdNig_WWcJDWWntF-p5gN2-P-CNTLxEa0/export?format=csv")
+    .then((response) => response.text())
+    .then((csv) => {
+      // console.log(csv)
+      const rows = Papa.parse(csv, { header: true })
+      // console.log(rows)
+      const geojsonFeatures = rows.data.map((row) => {
+        return {
+          type: "Feature",
+          properties: row,
+          geometry: {
+            type: "Point",
+            coordinates: [row.lon, row.lat]
+          }
+        }
+      })
+
+      const geojson = {
+        type: "FeatureCollection",
+        features: geojsonFeatures
+      }
+
+      map.addSource("vacancies", {
+        type: "geojson",
+        data: geojson,
+        cluster: true,
+        clusterRadius: 20
+      })
+
+      map.addLayer({
+        id: "clusters",
+        source: "vacancies",
+        type: "circle",
+        paint: {
+          "circle-color": [
+            "step", ["get", "point_count"],
+            "#7EC8E3",
+            3,
+            "#F9A03F",
+            6,
+            "#D7263D"
+          ],
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#FFFFFF",
+          "circle-radius": [
+            "step", ["get", "point_count"],
+            12,
+            3,
+            20,
+            6,
+            30
+          ]
+        }
+      })
+
+      map.addLayer({
+        id: "cluster-labels",
+        type: "symbol",
+        source: "vacancies",
+        layout: {
+          "text-field": ["get", "point_count"],
+          "text-size": 10
+        }
+      })
+
+      geojson.features.map((f) => {
+        document.getElementById("list-all").innerHTML += `<div class="list-item">
+                <h4>${f.properties["Вакансия"]}</h4>
+                <a href="#" onclick="map.flyTo({ center: [${f.geometry.coordinates}], zoom: 10})">Найти на карте</a>
+                </div><hr>`
+      })
+
+      map.on("idle", () => {
+        const features = map.queryRenderedFeatures({ layers: ["clusters"] })
+        document.getElementById("list-selected").innerHTML = "<h2>Сейчас на карте</h2>"
+
+        features.map(f => {
+          if (f.properties.cluster) {
+            map.getSource("vacancies").getClusterLeaves(
+              clusterId = f.properties.cluster_id,
+              limit = f.properties.point_count,
+              offset = 0
+            )
+              .then((clusterFeatures) => {
+                clusterFeatures.map((feature) => document.getElementById("list-selected")
+                  .innerHTML += `<div class="list-item">
+                                <h4>${feature.properties["Вакансия"]}</h4>
+                                <a target="blank_" href='${feature.properties["Ссылка на сайте Картетики"]}'>Подробнее</a>
+                                </div><hr>`)
+              })
+          } else {
+            document.getElementById("list-selected")
+              .innerHTML += `<div class="list-item">
+                                <h4>${f.properties["Вакансия"]}</h4>
+                                <a target="blank_" href='${f.properties["Ссылка на сайте Картетики"]}'>Подробнее</a>
+                                </div><hr>`
+          }
+        })
+      })
+    })
+
+  map.on("click", "clusters", function (e) {
+    map.flyTo({ center: e.lngLat, zoom: 8 });
+  })
+
+  map.on("mouseenter", "clusters", function () {
+    map.getCanvas().style.cursor = "pointer";
+  })
+
+  map.on("mouseleave", "clusters", function () {
+    map.getCanvas().style.cursor = "";
+  })
+})
